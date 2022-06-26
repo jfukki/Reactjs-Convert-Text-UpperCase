e3cc64c05e796475574ae55fa24ae1871abb81a1
Farrukh Tahir Malek <jfukki@gmail.com>
2022-06-26 16:33:03 +0500

    Add files via upload
    React Text - U

---
+++ precache-manifest.d9ce944eff0af9844afe79280c691341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeda692db2c91c9e0e30",
+    "url": "/static/css/main.56ade06d.chunk.css"
+  },
+  {
+    "revision": "aeda692db2c91c9e0e30",
+    "url": "/static/js/main.aeda692d.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "d8858afec4544fba18d3",
+    "url": "/static/js/2.d8858afe.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "f4f145677304f07b29170cc135800241",
+    "url": "/index.html"
+  }
+];
